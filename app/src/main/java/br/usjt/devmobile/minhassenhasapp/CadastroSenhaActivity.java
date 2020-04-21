dfbacd5880323c75ad04a39bdd746d9e0372e48c
luansena1994 <luan.nascimento1994@hotmail.com>
2020-04-20 21:23:05 -0300

    validação da senha e detalhes da senha

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroSenhaActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroSenhaActivity.java
@@ -2,11 +2,15 @@ package br.usjt.devmobile.minhassenhasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+import androidx.room.util.StringUtil;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Optional;
 
 
 public class CadastroSenhaActivity extends AppCompatActivity {
@@ -14,7 +18,7 @@ public class CadastroSenhaActivity extends AppCompatActivity {
     private static final String TAG = "CadastroSenhaActivity";
     private TextInputEditText nome;
     private TextInputEditText usuario;
-    private TextInputEditText senha;
+    private TextInputEditText senha1;
     private TextInputEditText url;
     private TextInputEditText observacao;
     private AppDatabase db;
@@ -27,22 +31,33 @@ public class CadastroSenhaActivity extends AppCompatActivity {
                 AppDatabase.class, "database-name").build();
         nome = (TextInputEditText)findViewById(R.id.cadNomeSenhaEditTextInput);
         usuario = (TextInputEditText)findViewById(R.id.cadUsuarioEditTextInput);
-        senha = (TextInputEditText)findViewById(R.id.cadSenhaEditTextInput);
+        senha1 = (TextInputEditText)findViewById(R.id.cadSenhaEditTextInput);
         url = (TextInputEditText)findViewById(R.id.cadUrlEditTextInput);
         observacao = (TextInputEditText)findViewById(R.id.cadObservacaoEditTextInput);
     }
 
+
     public void cadastrarSenha(View view) {
 
-        final Senha senha = new Senha(
-                nome.getText().toString(),
-                usuario.getText().toString(),
-                this.senha.getText().toString(),
-                url.getText().toString(),
-                observacao.getText().toString()
-                );
+        final Senha senha = new Senha();
+        if(!nome.getText().toString().isEmpty() &&
+        !usuario.getText().toString().isEmpty() &&
+        !senha1.getText().toString().isEmpty()){
+            senha.setNome(nome.getText().toString());
+            senha.setUsuario(usuario.getText().toString());
+            senha.setSenha(senha1.getText().toString());
+            senha.setUrl(url.getText().toString());
+            senha.setObservacao(observacao.getText().toString());
+            new SaveSenhaAsyncTask().execute(senha);
+        }else{
+            nome.setError("campo obrigatório");
+            usuario.setError("campo obrigatório");
+            senha1.setError("campo obrigatório");
+        }
 
-                new SaveSenhaAsyncTask().execute(senha);
+
+
+
     }
 
 
@@ -50,8 +65,8 @@ public class CadastroSenhaActivity extends AppCompatActivity {
     {
         @Override
         protected Boolean doInBackground(Senha... senha) {
-           db.senhaDao().insertAll(senha);
-           return true;
+            db.senhaDao().insertAll(senha);
+            return true;
         }
 
         @Override
--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
@@ -9,7 +9,13 @@ import android.widget.TextView;
 public class DetalhesSenhaActivity extends AppCompatActivity {
 
     private Senha senha;
-    private TextView textViewSenha;
+    private TextView textViewNome;
+    private TextView textViewUsuario;
+    private TextView textViewSenha1;
+    private TextView textViewUrl;
+    private TextView textViewObservacao;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,16 @@ public class DetalhesSenhaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhe_senha);
         Intent intent = getIntent();
         senha = (Senha)intent.getSerializableExtra("senha");
-        textViewSenha = findViewById(R.id.textViewNomeValue);
-        textViewSenha.setText(senha.getNome());
+        textViewNome = findViewById(R.id.textViewNomeValue);
+        textViewUsuario = findViewById(R.id.textViewUsuarioValue);
+        textViewSenha1 = findViewById(R.id.textViewSenha1Value);
+        textViewUrl = findViewById(R.id.textViewUrlValue);
+        textViewObservacao = findViewById(R.id.textViewObsValue);
+        textViewNome.setText(senha.getNome());
+        textViewUsuario.setText(senha.getUsuario());
+        textViewSenha1.setText(senha.getSenha());
+        textViewUrl.setText(senha.getUrl());
+        textViewObservacao.setText(senha.getObservacao());
+
     }
 }
